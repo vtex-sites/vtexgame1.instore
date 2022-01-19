28ccf2ccedc5dd21587073d11dfc85ef70618197
Guilherme Bruzzi <guibruzzi@gmail.com>
2022-01-19 18:54:05 -0300

    Add gatsby-plugin-no-sourcemaps

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,8 @@ module.exports = {
       resolve: '@vtexlab/gatsby-theme-instore-core',
       options: { tenant: STORE_ID },
     },
+    {
+      resolve: 'gatsby-plugin-no-sourcemaps',
+    },
   ],
 }
